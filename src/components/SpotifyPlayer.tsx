a6489932b37a347eaf2d12e0d82336f085a51c47
ice0622 <ayase5666@gmail.com>
2025-03-19 09:49:43 +0900

    型定義の修正 #2

--- src/components/SpotifyPlayer.tsx
+++ src/components/SpotifyPlayer.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 interface SpotifyPlayerProps {
   playlistId: string;
-  width?: string | boolean;
-  height?: string | boolean;
+  width?: string | number;
+  height?: string | number;
 }
 
 const SpotifyPlayer: React.FC<SpotifyPlayerProps> = ({ 
