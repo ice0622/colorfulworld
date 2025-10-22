e465f90bb6a0883d29e37cd3f2dbf68587d185f0
ice0622 <ayase5666@gmail.com>
2025-10-22 10:22:23 +0900

    aboutmeを.mdファイルに分割

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,52 +1,40 @@
+import fs from "fs";
+import path from "path";
+import { Metadata } from "next";
+import Markdown from "react-markdown";
+
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { config } from "@/config";
 import { signOgImageUrl } from "@/lib/og-image";
-import Markdown from "react-markdown";
 
-const content = `
-### 自己紹介
-
-彩世です
-
-2001年生まれです
-
-NikonFEとiPhone 15 Proをよく使っています
-
-写真と旅行とゲームとバイクに乗るのが好きです
-
-自分が好きな時に更新していきます
-
-`;
-
-export async function generateMetadata() {
-  return {
+export const metadata: Metadata = {
+  title: "About Me",
+  description: "Learn more about Ayase and her travel adventures",
+  openGraph: {
     title: "About Me",
     description: "Learn more about Ayase and her travel adventures",
-    openGraph: {
-      title: "About Me",
-      description: "Learn more about Ayase and her travel adventures",
-      images: [
-        signOgImageUrl({
-          title: "Ayase",
-          label: "About Me",
-          brand: config.blog.name,
-        }),
-      ],
-    },
-  };
-}
+    images: [
+      signOgImageUrl({
+        title: "Ayase",
+        label: "About Me",
+        brand: config.blog.name,
+      }),
+    ],
+  },
+};
 
-const Page = async () => {
+const aboutPath = path.join(process.cwd(), "content/about.md");
+const content = fs.readFileSync(aboutPath, "utf-8");
+
+export default function Page() {
   return (
     <div className="container mx-auto px-5">
       <Header />
-      <div className="prose lg:prose-lg dark:prose-invert m-auto mt-20 mb-10 blog-content">
+      <article className="prose lg:prose-lg dark:prose-invert mx-auto mt-20 mb-10 blog-content">
         <Markdown>{content}</Markdown>
-      </div>
+      </article>
       <Footer />
     </div>
   );
-};
-
-export default Page;
+}
